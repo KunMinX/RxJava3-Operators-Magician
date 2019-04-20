53a4b7df503682019f77b2d3d09cf4e6a5642bc4
KunMinX <kunminx@gmail.com>
2019-04-20 18:18:21 +0800

    drawer update

--- app/src/main/java/com/kunminx/rxmagic/MainActivity.java
+++ app/src/main/java/com/kunminx/rxmagic/MainActivity.java
@@ -1,26 +1,10 @@
 package com.kunminx.rxmagic;
 
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.ImageView;
 
-import com.google.android.material.navigation.NavigationView;
 import com.kunminx.rxmagic.databinding.ActivityMainBinding;
-import com.kunminx.samples.MyApplication;
-import com.kunminx.samples.ui.OperatorsActivity;
-import com.kunminx.samples.ui.cache.CacheExampleActivity;
-import com.kunminx.samples.ui.compose.ComposeOperatorExampleActivity;
-import com.kunminx.samples.ui.networking.NetworkingActivity;
-import com.kunminx.samples.ui.pagination.PaginationActivity;
-import com.kunminx.samples.ui.rxbus.RxBusActivity;
-import com.kunminx.samples.ui.search.SearchActivity;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -32,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mBinding;
     private RxMagicFragment mRxMagicFragment;
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,69 +24,12 @@ public class MainActivity extends AppCompatActivity {
         loadMagicFragment();
 
 
-        ImageView ivHead = (ImageView) mBinding.navView.getHeaderView(0);
-//        Glide.with(this).load("").centerCrop().into(ivHead);
-        mBinding.navView.setBackgroundTintList(getDefaultTint());
-        mBinding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.nav_declare:
-
-                        break;
-                    case R.id.nav_operators:
-                        startActivity(new Intent(MainActivity.this, OperatorsActivity.class));
-                        break;
-                    case R.id.nav_networking:
-                        startActivity(new Intent(MainActivity.this, NetworkingActivity.class));
-                        break;
-                    case R.id.nav_cache:
-                        startActivity(new Intent(MainActivity.this, CacheExampleActivity.class));
-                        break;
-                    case R.id.nav_rxbus:
-                        ((MyApplication) getApplication()).sendAutoEvent();
-                        startActivity(new Intent(MainActivity.this, RxBusActivity.class));
-                        break;
-                    case R.id.nav_pagination:
-                        startActivity(new Intent(MainActivity.this, PaginationActivity.class));
-                        break;
-                    case R.id.nav_compose:
-                        startActivity(new Intent(MainActivity.this, ComposeOperatorExampleActivity.class));
-                        break;
-                    case R.id.nav_search:
-                        startActivity(new Intent(MainActivity.this, SearchActivity.class));
-                        break;
-                    default:
-                }
-                return true;
-            }
-        });
-    }
-
-    //TODO
-    private ColorStateList getDefaultTint() {
-        int[] colors = new int[]{
-                getResources().getColor(R.color.colorGray),
-                getResources().getColor(R.color.colorGray),
-                getResources().getColor(R.color.colorGray),
-                getResources().getColor(R.color.colorGray),
-                getResources().getColor(R.color.colorGray),
-                getResources().getColor(R.color.colorGray)
-        };
-        int[][] states = new int[6][];
-        states[0] = new int[]{android.R.attr.state_checked, android.R.attr.state_enabled};
-        states[1] = new int[]{android.R.attr.state_enabled, android.R.attr.state_focused};
-        states[2] = new int[]{android.R.attr.state_enabled};
-        states[3] = new int[]{android.R.attr.state_focused};
-        states[4] = new int[]{android.R.attr.state_window_focused};
-        states[5] = new int[]{};
-        return new ColorStateList(states, colors);
     }
 
     private void loadMagicFragment() {
         mRxMagicFragment = RxMagicFragment.newInstance();
         getSupportFragmentManager().beginTransaction()
-                .add(mRxMagicFragment, mRxMagicFragment.getClass().getSimpleName())
+                .add(R.id.fragment_container, mRxMagicFragment)
                 .addToBackStack(null).commit();
     }
 
--- app/src/main/java/com/kunminx/rxmagic/RxMagicFragment.java
+++ app/src/main/java/com/kunminx/rxmagic/RxMagicFragment.java
@@ -1,14 +1,35 @@
 package com.kunminx.rxmagic;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
 import com.kunminx.rxmagic.databinding.FragmentRxmagicBinding;
+import com.kunminx.samples.MyApplication;
+import com.kunminx.samples.ui.OperatorsActivity;
+import com.kunminx.samples.ui.cache.CacheExampleActivity;
+import com.kunminx.samples.ui.compose.ComposeOperatorExampleActivity;
+import com.kunminx.samples.ui.networking.NetworkingActivity;
+import com.kunminx.samples.ui.pagination.PaginationActivity;
+import com.kunminx.samples.ui.rxbus.RxBusActivity;
+import com.kunminx.samples.ui.search.SearchActivity;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 
 /**
@@ -34,9 +55,83 @@ public class RxMagicFragment extends Fragment {
         return view;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+//        ImageView ivHead = (ImageView) mBinding.navView.inflateHeaderView(R.layout.layout_drawer_header);
+//        ImageView imageView = (ImageView) ivHead.findViewById(R.id.iv_drawer);
+//        Glide.with(this).load(getResources().getDrawable(R.drawable.bg_head)).into(ivHead);
+        mBinding.navView.setBackgroundTintList(getDefaultTint());
+        mBinding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_declare:
+                        mBinding.drawer.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.nav_operators:
+                        startActivity(new Intent(getActivity(), OperatorsActivity.class));
+                        break;
+                    case R.id.nav_networking:
+                        startActivity(new Intent(getActivity(), NetworkingActivity.class));
+                        break;
+                    case R.id.nav_cache:
+                        startActivity(new Intent(getActivity(), CacheExampleActivity.class));
+                        break;
+                    case R.id.nav_rxbus:
+                        //TODO
+                        ((MyApplication) getActivity().getApplication()).sendAutoEvent();
+                        startActivity(new Intent(getActivity(), RxBusActivity.class));
+                        break;
+                    case R.id.nav_pagination:
+                        startActivity(new Intent(getActivity(), PaginationActivity.class));
+                        break;
+                    case R.id.nav_compose:
+                        startActivity(new Intent(getActivity(), ComposeOperatorExampleActivity.class));
+                        break;
+                    case R.id.nav_search:
+                        startActivity(new Intent(getActivity(), SearchActivity.class));
+                        break;
+                    default:
+                }
+                return true;
+            }
+        });
+    }
 
+    //TODO
+    private ColorStateList getDefaultTint() {
+        int[] colors = new int[]{
+                getResources().getColor(R.color.colorWhite),
+                getResources().getColor(R.color.colorWhite),
+                getResources().getColor(R.color.colorWhite),
+                getResources().getColor(R.color.colorWhite),
+                getResources().getColor(R.color.colorWhite),
+                getResources().getColor(R.color.colorWhite)
+        };
+        int[][] states = new int[6][];
+        states[0] = new int[]{android.R.attr.state_checked, android.R.attr.state_enabled};
+        states[1] = new int[]{android.R.attr.state_enabled, android.R.attr.state_focused};
+        states[2] = new int[]{android.R.attr.state_enabled};
+        states[3] = new int[]{android.R.attr.state_focused};
+        states[4] = new int[]{android.R.attr.state_window_focused};
+        states[5] = new int[]{};
+        return new ColorStateList(states, colors);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.rxmagic_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+//            case android.
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
