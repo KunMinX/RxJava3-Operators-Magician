7985fe630b961c91d5b622a3d1a8dd903f3d97ea
KunMinX <kunminx@gmail.com>
2019-04-28 10:46:29 +0800

    fix rxExpressionAdapter editText cache bug

--- app/src/main/java/com/kunminx/rxmagic/ui/adapter/RxExpressionAdapter.java
+++ app/src/main/java/com/kunminx/rxmagic/ui/adapter/RxExpressionAdapter.java
@@ -18,15 +18,17 @@ package com.kunminx.rxmagic.ui.adapter;
 
 import android.content.Context;
 import android.text.Editable;
-import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.SparseArray;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.kunminx.rxmagic.R;
 import com.kunminx.rxmagic.bean.RxExpression;
 import com.kunminx.rxmagic.databinding.AdapterRxExpressionBinding;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 /**
@@ -36,6 +38,8 @@ public class RxExpressionAdapter extends BaseBindingAdapter<RxExpression, Adapte
 
     private SparseArray<String> mTextCache = new SparseArray<>();
     private OnItemClickListener mListener;
+    private int mEtFocusPosition = -1;
+    private InputMethodManager inputMethodManager = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
 
     public RxExpressionAdapter(Context context) {
         super(context);
@@ -48,33 +52,13 @@ public class RxExpressionAdapter extends BaseBindingAdapter<RxExpression, Adapte
 
     @Override
     protected void onBindItem(AdapterRxExpressionBinding binding, RxExpression item, RecyclerView.ViewHolder holder) {
-
         binding.btnOp.setText(item.getRxOperator().getName());
         binding.et.setText(mTextCache.get(holder.getAdapterPosition()));
-
-        if ((binding.et.getTag() != null && (boolean) binding.et.getTag())) {
-            return;
-        }
-
-        binding.et.addTextChangedListener(new TextWatcher() {
+        binding.et.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (TextUtils.equals(mTextCache.get(holder.getAdapterPosition()), s.toString())) {
-                    return;
-                }
-                mTextCache.put(holder.getAdapterPosition(), s.toString());
-                if (binding.et.getText() != null) {
-                    item.setExpression(binding.et.getText().toString());
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    mEtFocusPosition = holder.getAdapterPosition();
                 }
             }
         });
@@ -82,6 +66,48 @@ public class RxExpressionAdapter extends BaseBindingAdapter<RxExpression, Adapte
         binding.btnOp.setOnClickListener(v -> {
             mListener.onItemClick(v, item, holder.getAdapterPosition());
         });
+    }
+
+    private TextWatcher mTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            mTextCache.put(mEtFocusPosition, s.toString());
+            mList.get(mEtFocusPosition).setExpression(s.toString());
+        }
+    };
+
+    @Override
+    public void onViewAttachedToWindow(@NonNull RecyclerView.ViewHolder holder) {
+        super.onViewAttachedToWindow(holder);
+        AdapterRxExpressionBinding binding = DataBindingUtil.getBinding(holder.itemView);
+        binding.et.addTextChangedListener(mTextWatcher);
+
+        if (mEtFocusPosition == holder.getAdapterPosition()) {
+            binding.et.requestFocus();
+            binding.et.setSelection(binding.et.getText().length());
+        }
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(@NonNull RecyclerView.ViewHolder holder) {
+        super.onViewDetachedFromWindow(holder);
+        AdapterRxExpressionBinding binding = DataBindingUtil.getBinding(holder.itemView);
+        binding.et.removeTextChangedListener(mTextWatcher);
+
+        binding.et.clearFocus();
+        if (mEtFocusPosition == holder.getAdapterPosition()) {
+            inputMethodManager.hideSoftInputFromWindow(binding.et.getWindowToken(), 0);
+        }
     }
 
     public void setOnButtonClickListener(OnItemClickListener listener) {
